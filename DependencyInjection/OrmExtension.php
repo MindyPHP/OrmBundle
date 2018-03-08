43d1eb91faa544861a80a3199f949b229c64c4c4
Falaleev Maxim <max@studio107.ru>
2018-03-08 14:58:44 +0300

    Fix tests

--- DependencyInjection/OrmExtension.php
+++ DependencyInjection/OrmExtension.php
@@ -3,8 +3,7 @@
 declare(strict_types=1);
 
 /*
- * This file is part of Mindy Framework.
- * (c) 2018 Maxim Falaleev
+ * Studio 107 (c) 2018 Maxim Falaleev
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -34,7 +33,7 @@ class OrmExtension extends Extension
         $connectionManagerDefinition = $container->getDefinition(ConnectionManager::class);
         $connectionManagerDefinition->setArguments([
             $config['connections'],
-            $config['default_connection']
+            $config['default_connection'],
         ]);
     }
 }
